c6eb1a119616dc5732e83f3f1ae903d499fea670
Arron Harvey <arron.harvey@icloud.com>
2023-07-03 13:33:36 +1000

    Initial Commit

---
+++ Github Demo/Github_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_DemoApp.swift
+//  Github Demo
+//
+//  Created by Arron on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
