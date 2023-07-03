c02bb7bbf99d84a421abfb977d0e2625570ec959
Arron Harvey <arron.harvey@icloud.com>
2023-07-03 13:50:35 +1000

    Added Second Page

---
+++ Github Demo/Second View.swift
@@ -0,0 +1,20 @@
+//
+//  Second View.swift
+//  Github Demo
+//
+//  Created by Arron on 3/7/2023.
+//
+
+import SwiftUI
+
+struct Second_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Second_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Second_View()
+    }
+}
